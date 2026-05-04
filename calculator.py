97a565c64cc5289363bb9a37e3638a10f6bdffd8
kamalkharel5 <kamalkharel5@gmail.com>
2026-05-04 13:43:32 -0400

    Add multiply

--- calculator.py
+++ calculator.py
@@ -6,7 +6,13 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print("Calculator loaded!")
     print(f"5 + 3 = {add(5, 3)}")
     print(f"10 - 4 = {subtract(10, 4)}")
+
+
+
